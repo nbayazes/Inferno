00a339228b1cc249995d5cc7d38448a12ddc2c2b
Nicholas Bayazes <nicholas.bayazes@gmail.com>
2023-04-01 12:22:33 -0400

    Move bank smoothing to physics data

--- src/Inferno.Core/Object.h
+++ src/Inferno.Core/Object.h
@@ -159,6 +159,42 @@ namespace Inferno {
     // Object signature
     enum class ObjSig : uint16 {};
 
+
+    // Animates a value using second order dynamics
+    template <class T>
+    class SecondOrderDynamics {
+        // https://www.youtube.com/watch?v=KPoeNZZ6H4s
+        float _k1, _k2, _k3;
+        T _prevValue;
+        T _y, _yd = {};
+
+    public:
+        // f: Frequency response speed
+        // zeta: settling -> 0 is undamped. 0..1 underdamped. 1 > no vibration. 1 is critical dampening
+        // r: response ramping. 0..1 input is delayed. 1: immediate response >1: overshoots target  <0 predicts movement
+        SecondOrderDynamics(float f = 1, float z = 1, float r = 0, T initialValue = {})
+            : _k1(z / (DirectX::XM_PI * f)),
+            _k2(1 / std::pow(DirectX::XM_2PI * f, 2.0f)),
+            _k3(r* z / (DirectX::XM_2PI * f)) {
+            _prevValue = initialValue;
+            _y = initialValue;
+        }
+
+        // Updates the value
+        T Update(T value, T velocity, float dt /*delta time*/) {
+            _y += dt * _yd; // integrate by velocity
+            _yd += dt * (value + _k3 * velocity - _y - _k1 * _yd) / _k2; // integrate velocity by acceleration
+            return _y;
+        }
+
+        // Updates the value using an estimated velocity
+        T Update(T value, float dt) {
+            T velocity = (value - _prevValue) / dt; // estimate velocity from previous state
+            _prevValue = value;
+            return Update(value, velocity, dt);
+        }
+    };
+
     struct PhysicsData {
         Vector3 Velocity;
         Vector3 InputVelocity; // Velocity after input but before collision
@@ -174,6 +210,7 @@ namespace Inferno {
         int Bounces = 0; // Number of remaining bounces
         float Wiggle = 0; // Amplitude of wiggle
         float WiggleRate = 1; // How long one wiggle takes
+        SecondOrderDynamics<float> BankState = { 1, 1, 0, 0 };
 
         bool CanBounce() const {
             return Bounces > 0 || HasFlag(Flags, PhysicsFlag::Bounce);
--- src/Inferno/Physics.cpp
+++ src/Inferno/Physics.cpp
@@ -237,43 +237,6 @@ namespace Inferno {
         return true; // was destroyed!
     }
 
-    // Animates a value using second order dynamics
-    template <class T>
-    class SecondOrderDynamics {
-        // https://www.youtube.com/watch?v=KPoeNZZ6H4s
-        const float _k1, _k2, _k3;
-        T _prevValue;
-        T _y, _yd = {};
-
-    public:
-        // f: Frequency response speed
-        // zeta: settling -> 0 is undamped. 0..1 underdamped. 1 > no vibration. 1 is critical dampening
-        // r: response ramping. 0..1 input is delayed. 1: immediate response >1: overshoots target  <0 predicts movement
-        SecondOrderDynamics(float f = 1, float z = 1, float r = 0, T initialValue = {})
-            : _k1(z / (XM_PI * f)),
-              _k2(1 / std::pow(XM_2PI * f, 2.0f)),
-              _k3(r * z / (XM_2PI * f)) {
-            _prevValue = initialValue;
-            _y = initialValue;
-        }
-
-        // Updates the value
-        T Update(T value, T velocity, float dt /*delta time*/) {
-            _y += dt * _yd; // integrate by velocity
-            _yd += dt * (value + _k3 * velocity - _y - _k1 * _yd) / _k2; // integrate velocity by acceleration
-            return _y;
-        }
-
-        // Updates the value using an estimated velocity
-        T Update(T value, float dt) {
-            T velocity = (value - _prevValue) / dt; // estimate velocity from previous state
-            _prevValue = value;
-            return Update(value, velocity, dt);
-        }
-    };
-
-    SecondOrderDynamics<float> BankState = { 1, 1, 0, 0 }; // hack: only works when there's one object with banking
-
     // Rolls the object when turning
     void TurnRoll(PhysicsData& pd, float rollScale, float rollRate, float dt) {
         const auto desiredBank = pd.AngularVelocity.y * rollScale;
@@ -289,7 +252,7 @@ namespace Inferno {
                 roll = -roll;
         }
 
-        pd.TurnRoll = BankState.Update(roll, dt);
+        pd.TurnRoll = pd.BankState.Update(roll, dt);
     }
 
     // Applies angular physics to the player
