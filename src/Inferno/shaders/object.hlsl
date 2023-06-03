cca0f7c03823c931d033be118dd6ff27d77e0606
Nicholas Bayazes <nicholas.bayazes@gmail.com>
2023-06-03 15:10:52 -0400

    Update object shader constant count

--- src/Inferno/shaders/object.hlsl
+++ src/Inferno/shaders/object.hlsl
@@ -4,7 +4,7 @@
 #define RS "RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT), "\
     "CBV(b0),"\
     "DescriptorTable(SRV(t0, space = 1, numDescriptors = unbounded, flags = DESCRIPTORS_VOLATILE), visibility=SHADER_VISIBILITY_PIXEL), " \
-    "RootConstants(b1, num32BitConstants = 37), "\
+    "RootConstants(b1, num32BitConstants = 26), "\
     "DescriptorTable(SRV(t0, numDescriptors = 5), visibility=SHADER_VISIBILITY_PIXEL), " \
     "DescriptorTable(SRV(t5), visibility=SHADER_VISIBILITY_PIXEL), " \
     "DescriptorTable(SRV(t6), visibility=SHADER_VISIBILITY_PIXEL), " \
