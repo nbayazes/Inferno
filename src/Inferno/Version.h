2c677966ac488e6a95f8483e10987a850fcb8cf9
Nicholas Bayazes <nicholas.bayazes@gmail.com>
2023-07-10 10:19:59 -0400

    Version 1.0.17

--- src/Inferno/Version.h
+++ src/Inferno/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace Inferno {
-    constexpr auto VERSION_STRING = "1.0.16";
+    constexpr auto VERSION_STRING = "1.0.17";
     constexpr auto APP_TITLE = "Inferno Editor";
 }
