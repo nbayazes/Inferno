1483bd988948ea2d066af8e3f0a5f2cb2f560dfe
Nicholas Bayazes <nicholas.bayazes@gmail.com>
2023-07-28 13:59:06 -0400

    Also allow numpad enter for closing dialogs

--- src/Inferno/Editor/UI/WindowBase.h
+++ src/Inferno/Editor/UI/WindowBase.h
@@ -202,7 +202,7 @@ namespace Inferno::Editor {
                     if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape)))
                         Close();
 
-                    if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Enter)) && !ImGui::GetIO().WantTextInput)
+                    if ((ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Enter)) || ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_KeypadEnter))) && !ImGui::GetIO().WantTextInput)
                         Close(true);
                 }
 
