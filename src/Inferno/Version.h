fd37eb6b70afa4ebdc50ba09ca636708807a0f35
Nicholas Bayazes <nicholas.bayazes@gmail.com>
2024-01-08 12:32:26 -0500

    Change app titling

--- src/Inferno/Version.h
+++ src/Inferno/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace Inferno {
-    constexpr auto VERSION_STRING = "1.0.17";
-    constexpr auto APP_TITLE = "Inferno Editor";
+    constexpr auto VERSION_STRING = "0.1.0";
+    constexpr auto APP_TITLE = "Inferno [ALPHA]";
 }
