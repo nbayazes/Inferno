209e1cd5e72a06b35bbecd5fb4020714b0906a86
Nicholas Bayazes <nicholas.bayazes@gmail.com>
2025-07-26 18:23:52 -0400

    Version 0.4.2

--- src/Inferno/Version.h
+++ src/Inferno/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace Inferno {
-    constexpr auto VERSION_STRING = "0.4.1b";
-    constexpr auto APP_TITLE = "Inferno 0.4.1b [BETA]";
+    constexpr auto VERSION_STRING = "0.4.2";
+    constexpr auto APP_TITLE = "Inferno 0.4.2 [BETA]";
 }
