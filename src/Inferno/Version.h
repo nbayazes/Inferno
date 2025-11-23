c4ec27e7b9743541a0ef12e078ad9cb2099da972
Nicholas Bayazes <nicholas.bayazes@gmail.com>
2025-11-23 08:21:52 -0500

    Version 1.0.28

--- src/Inferno/Version.h
+++ src/Inferno/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace Inferno {
-    constexpr auto VERSION_STRING = "1.0.27";
-    constexpr auto APP_TITLE = "Inferno Editor 1.0.27";
+    constexpr auto VERSION_STRING = "1.0.28";
+    constexpr auto APP_TITLE = "Inferno Editor 1.0.28";
 }
